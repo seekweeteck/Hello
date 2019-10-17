8996d05de0e80f7f31cd4c609dda5ba45cdd23b9
seekweeteck <seekt@tarc.edu.my>
2019-10-17 15:54:24 +0800

    Initial

---
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+// : = extends
+class MainActivity : AppCompatActivity() {
+
+    //onCreate = main()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Create the UI
+        //R = resources
+        setContentView(R.layout.activity_main)
+        //TODO : Continue here
+        //Java int x; vs Kotlin var x : Int
+        //val = value and var = variable
+        //findViewByID = link UI to code
+        val buttonDonClickMe : Button = findViewById(R.id.buttonDonClickMe)
+        buttonDonClickMe.setOnClickListener { showMessage() }
+
+    }
+
+    private fun showMessage() {
+        textViewMessage.setText(getString(R.string.warning_message))
+    }
+}
